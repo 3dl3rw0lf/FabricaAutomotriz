aebe907d0205b6662974b0319eab48bbde5b9691
Gustavo Arauz <arauz.gus@gmail.com>
2024-10-06 20:29:22 -0300

    GUI completion
    Se completa GUI con elementos visuales

--- src/GUI.java
+++ src/GUI.java
@@ -17,23 +17,34 @@ import java.util.ArrayList;
 
 public class GUI extends JFrame {
     private JPanel panel;
-    // private JTextField cantidadCilindrosTextField;
-    private JTextField cilindradaTextField;
-    private JTextField textField3;
+
+    private JRadioButton claseAuto;
+    private JRadioButton claseInventario;
+
+    private JLabel motorJLabel;
+    private JLabel cantidadDeCilindrosLabel;
+    private JComboBox<String> cantidadCilindrosComboBox;
+    private JLabel cilindradaLabel;
+    private JComboBox cilindradaComboBox;
+    private JLabel precioMotorJLabel;
+    private JSpinner precioMotorJSpinner;
+
+    private JLabel carroceriaJLabel;
+    private JLabel descripcionCarroceriaJlabel;
+    private JComboBox descripcionCarroceriaComboBox;
+    private JLabel colorCarroceriaJLabel;
+    private JComboBox colorCarroceriaComboBox;
+    private JSpinner precioCarroceriaJSpinner;
+    private JLabel precioCarroceriaJLabel;
+
     private JLabel marcaLabel;
     private JTextField marcaTextField;
     private JTextArea textVehiculos;
-    private JRadioButton claseAuto;
-    private JRadioButton claseInventario;
     private JButton agregarAutoAlInventarioButton;
     private JButton mostrarInventarioButton;
     private JList inventariosList;
     private DefaultListModel<String> listModel;
     private JButton agregarAlInventarioButton;
-    private JLabel cantidadDeCilindrosLabel;
-    private JLabel tres;
-    private JLabel cilindradaLabel;
-    private JComboBox<String> cantidadCilindrosComboBox;
 
     //TODO: hacer muchos inventarios
     private ArrayList<Inventario> inventarios = new ArrayList<>();
@@ -43,24 +54,53 @@ public class GUI extends JFrame {
         super("Fábrica Automotriz");
 
 
-        cantidadCilindrosComboBox.addItem("4");
-        cantidadCilindrosComboBox.addItem("6");
-        cantidadCilindrosComboBox.addItem("8");
-        cantidadCilindrosComboBox.addItem("12");
-        cantidadCilindrosComboBox.addItem("16");
+        cantidadCilindrosComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-        //  4 cilindros
-        // 1.3
-        // 1.4
-        // 1.6
-        // 1.8
-        // 2.0
-        // 2.5
+                cilindradaComboBox.removeAllItems();
 
-        // 6 cilindros
-        // 3.6
+                String seleccionCilindros = (String) cantidadCilindrosComboBox.getSelectedItem();
+
+                if (seleccionCilindros != null) {
+                    int cantidadCilindros = Integer.parseInt(seleccionCilindros);
+
+                    switch (cantidadCilindros) {
+                        case 4:
+                            cilindradaComboBox.addItem("1.3");
+                            cilindradaComboBox.addItem("1.4");
+                            cilindradaComboBox.addItem("1.6");
+                            cilindradaComboBox.addItem("1.8");
+                            cilindradaComboBox.addItem("2.0");
+                            cilindradaComboBox.addItem("2.5");
+                            break;
+                        case 6:
+                            cilindradaComboBox.addItem("2.7");
+                            cilindradaComboBox.addItem("3.0");
+                            cilindradaComboBox.addItem("3.6");
+                            break;
+                        case 8:
+                            cilindradaComboBox.addItem("5.0");
+                            cilindradaComboBox.addItem("6.6");
+                            break;
+                        case 12:
+                            cilindradaComboBox.addItem("7.0");
+                            cilindradaComboBox.addItem("8.4");
+                            break;
+                        case 16:
+                            cilindradaComboBox.addItem("10.0");
+                            cilindradaComboBox.addItem("12.0");
+                            break;
+                        default:
+                            cilindradaComboBox.addItem("N/A");
+                    }
+                }
+            }
+        });
+
+
         setContentPane(panel);
-        setSize(760, 319);
+        setSize(907, 358);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
@@ -120,41 +160,36 @@ public class GUI extends JFrame {
                 String selectedMarca = (String) inventariosList.getSelectedValue();
                 if (selectedMarca != null) {
                     JOptionPane.showMessageDialog(panel, "Inventario seleccionado: " + selectedMarca);
-                    // Aquí puedes agregar más lógica para mostrar detalles del inventario seleccionado si lo deseas
                 }
             }
         });
 
-        
     }
 
     private void inicializarVisibilidad(){
         claseAuto.setVisible(true);
         claseInventario.setVisible(true);
 
+        motorVisibleFalse();
+
+        carroceriaVisibleFalse();
+
+        agregarAutoAlInventarioButton.setVisible(false);
         marcaLabel.setVisible(false);
         marcaTextField.setVisible(false);
-        agregarAlInventarioButton.setVisible(false);
-        cantidadDeCilindrosLabel.setVisible(false);
-        cantidadCilindrosComboBox.setVisible(false);
-        cilindradaLabel.setVisible(false);
-        cilindradaTextField.setVisible(false);
-        tres.setVisible(false);
-        textField3.setVisible(false);
-        agregarAutoAlInventarioButton.setVisible(false);
         mostrarInventarioButton.setVisible(false);
         inventariosList.setVisible(false);
         textVehiculos.setVisible(false);
+        agregarAlInventarioButton.setVisible(false);
 
     }
 
     private void mostrarCamposAuto(){
-        cantidadDeCilindrosLabel.setVisible(true);
-        cantidadCilindrosComboBox.setVisible(true);
-        cilindradaLabel.setVisible(true);
-        cilindradaTextField.setVisible(true);
-        tres.setVisible(true);
-        textField3.setVisible(true);
+
+        motorVisibleTrue();
+
+        carroceriaVisibleTrue();
+
         agregarAutoAlInventarioButton.setVisible(true);
 
         marcaLabel.setVisible(false);
@@ -171,27 +206,59 @@ public class GUI extends JFrame {
         inventariosList.setVisible(true);
         mostrarInventarioButton.setVisible(true);
 
-        cantidadDeCilindrosLabel.setVisible(false);
-        cantidadCilindrosComboBox.setVisible(false);
-        cilindradaLabel.setVisible(false);
-        cilindradaTextField.setVisible(false);
-        tres.setVisible(false);
-        textField3.setVisible(false);
+        motorVisibleFalse();
+
+        carroceriaVisibleFalse();
+
         agregarAutoAlInventarioButton.setVisible(false);
     }
 
     private void createUIComponents() {
-        // Inicializar el componente JList
         inventariosList = new JList<>();
+    }
+
+    public void motorVisibleFalse(){
+        motorJLabel.setVisible(false);
+        cilindradaLabel.setVisible(false);
+        cilindradaComboBox.setVisible(false);
+        cantidadDeCilindrosLabel.setVisible(false);
+        cantidadCilindrosComboBox.setVisible(false);
+        precioMotorJLabel.setVisible(false);
+        precioMotorJSpinner.setVisible(false);
+    }
+
+    public void motorVisibleTrue(){
+        motorJLabel.setVisible(true);
+        cantidadDeCilindrosLabel.setVisible(true);
+        cantidadCilindrosComboBox.setVisible(true);
+        cilindradaLabel.setVisible(true);
+        cilindradaComboBox.setVisible(true);
+        precioMotorJLabel.setVisible(true);
+        precioMotorJSpinner.setVisible(true);
+    }
+
+    public void carroceriaVisibleFalse(){
+        carroceriaJLabel.setVisible(false);
+        descripcionCarroceriaJlabel.setVisible(false);
+        descripcionCarroceriaComboBox.setVisible(false);
+        colorCarroceriaJLabel.setVisible(false);
+        colorCarroceriaComboBox.setVisible(false);
+        precioCarroceriaJLabel.setVisible(false);
+        precioCarroceriaJSpinner.setVisible(false);
+    }
+
+    public void carroceriaVisibleTrue(){
+        carroceriaJLabel.setVisible(true);
+        descripcionCarroceriaJlabel.setVisible(true);
+        descripcionCarroceriaComboBox.setVisible(true);
+        colorCarroceriaJLabel.setVisible(true);
+        colorCarroceriaComboBox.setVisible(true);
+        precioCarroceriaJLabel.setVisible(true);
+        precioCarroceriaJSpinner.setVisible(true);
     }
 
     public static void main(String[] args) {
         JFrame panel = new GUI();
-/*        JFrame frame = new JFrame("Inventario GUI");
-        frame.setContentPane(new GUI().panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);*/
     }
 
 }
